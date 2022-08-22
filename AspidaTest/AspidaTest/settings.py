5a6f8537bb1f8dc379b8b4af1f882c0b8a5800a8
Brian <brianfiquitiva12@gmail.com>
2022-08-22 10:25:38 -0500

    first config deploy

--- AspidaTest/AspidaTest/settings.py
+++ AspidaTest/AspidaTest/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nx&fb5*+v(xxx^1y=)*8f9hyo&u#42fyywz=5=(i@g!ub+612u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOST')]
 
 
 # Application definition 
