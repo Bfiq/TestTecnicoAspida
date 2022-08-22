4b7b2ed68d9e1c45f018bac2ee886a1cf77d0887
Brian <brianfiquitiva12@gmail.com>
2022-08-22 02:03:26 -0500

    adding remote database with .env

--- AspidaTest/AspidaTest/settings.py
+++ AspidaTest/AspidaTest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'AspidaTest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #motor de la bd (no soporta las bds no relacionales)
-        'NAME': 'test', #nombre de la bd
-        "USER": 'root',
-        "PASSWORD": '',
-        "HOST": 'localhost',
-        "PORT" : '3306'
+        'NAME': config('DB_NAME'), #nombre de la bd
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT" : config('DB_PORT')
     }
 }
 
